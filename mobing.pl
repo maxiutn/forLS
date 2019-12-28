bd1d77a309fbf39546a98411687504474545f892
maxiutn <13839942+maxiutn@users.noreply.github.com>
2019-12-27 21:00:28 -0300

    Create mobing.pl

---
+++ mobing.pl
@@ -0,0 +1,21 @@
+$sharedWallet = box.getMoney()
+
+$Carlos = sharedWallet.percentage(80)
+$Raul = sharedWallet.percentage(80)
+$Fabio = sharedWallet.percentage(80)
+
+if ($Carlos){
+    print $Carlos;
+}
+else
+{
+  if ($Raul){
+    print $Raul;
+}
+else
+{
+  if ($Fabio){
+    print $Fabio;
+}
+
+}
